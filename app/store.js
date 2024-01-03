f2d473997f4be97da852fdd3d1bac486d50aba90
Artur Rodrigues <artur190503@outlook.com>
2024-01-03 14:27:21 -0300

    Usando Middleware na store

--- app/store.js
+++ app/store.js
@@ -1,4 +1,8 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+
+const reduxLogger = require('redux-logger')
+
+const logger = reduxLogger.createLogger()
 const cakeReducer = require('../features/cake/cakeSlice')
 const iceCreamReducer = require('../features/iceCream/iceCreamSlice')
 
@@ -6,7 +10,8 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         iceCream: iceCreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const { iceCreamActions } = require('./features/iceCream/iceCreamSlice')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 
 console.log('Estado inicial: ', store.getState())
-const unsubscribe = store.subscribe(() => {
-    console.log('Estado atualizado: ', store.getState())
-})
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
@@ -15,6 +13,7 @@ store.dispatch(iceCreamActions.ordered(5))
 store.dispatch(iceCreamActions.ordered(6))
 store.dispatch(iceCreamActions.ordered(9))
 store.dispatch(iceCreamActions.restocked(20))
+store.dispatch(cakeActions.ordered())
 
 unsubscribe()
 
