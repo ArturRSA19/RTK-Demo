21dd58f2e9c1895c3a4241ed0922b296ced96a22
Artur Rodrigues <artur190503@outlook.com>
2024-01-03 15:17:41 -0300

    Usando Extra Reducers para modificar dados através de actions de outros reducers

--- features/iceCream/iceCreamSlice.js
+++ features/iceCream/iceCreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require('../cake/cakeSlice')
+
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 const initialState = {
@@ -13,8 +15,13 @@ const iceCreamSlice = createSlice({
     },
     restocked: (state, action) => {
       state.numOfIceCreams += action.payload
-    }
-  }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, state => {
+        state.numOfIceCreams--
+    })
+  },
 })
 
 module.exports = iceCreamSlice.reducer
